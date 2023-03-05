896f207040e173ae482e3f6e7396460cccf699c3
ghada <100215621+engghadamohammed@users.noreply.github.com>
2023-03-05 13:01:50 +0200

    Add files via upload

---
+++ src/styles.js
@@ -0,0 +1,96 @@
+
+
+
+const styles= {
+  container: {
+    flex: 1,
+    backgroundColor: "#36B0F0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  avatar: {
+    width: "40%",
+    height: "20%",
+    margin: "3%",
+  },
+  input: {
+    backgroundColor: "#C5F1E7",
+    width: "80%",
+    height: 50,
+    margin: "3%",
+    padding: 5,
+    borderRadius: 8,
+    textAlign: "center",
+    fontSize: 20,
+  },
+  pressable: {
+    backgroundColor: "#2CE770",
+    width: "90%",
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "3%",
+    marginBottom: "3%",
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: "#79FA04",
+  },
+  startText: {
+    marginTop: "10%",
+    fontSize: 15,
+    color: "#fff",
+  },
+  body: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#60A5FA",
+  },
+  leftView: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    width: "50%",
+    margin: "2%",
+  },
+  rightView: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    width: "50%",
+    margin: "2%",
+  },
+  card: {
+    width: "100%",
+    height: "15.6%",
+    margin: "2%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: "50%",
+    height: "95%",
+    margin: 5,
+  },
+  cardText: {
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  cardImg: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#09B8F4",
+  },
+  topBar: {
+    flex: 1 / 15,
+    backgroundColor: "#1E3A8A",
+    justifyContent: "center",
+    padding: 8,
+  },
+} 
+export default styles
